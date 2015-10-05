b4143b6adf53e0e5977137de89e5da877bc74733
rizalwildan <rizal.wildan.a@ugm.mail.ac.id>
2015-10-05 14:05:46 +0700

    commit anayr

--- application/views/transaksi.php
+++ application/views/transaksi.php
@@ -20,7 +20,7 @@ Content Wrapper. Contains page content -->
                     <div class="col-sm-6">
                       <div id="example1_length" class="dataTables_length">
                         <label>
-                          Tahun Ajaran 
+                          Tahun Ajaran
                           <select class="form-control input-sm" aria-controls="example1" name="example1_length">
                             <option value="X1">--Pilih Tahun Ajaran--</option>
                             <option value="X2">2014/2015</option>
@@ -35,7 +35,7 @@ Content Wrapper. Contains page content -->
                     <div class="col-sm-6">
                       <div id="example1_length" class="dataTables_length">
                         <label>
-                          Transaksi Bulan 
+                          Transaksi Bulan
                           <select class="form-control input-sm" aria-controls="example1" name="example1_length">
                             <option value="X1">--Pilih Tahun Ajaran--</option>
                             <option value="X2">Juli</option>
@@ -49,122 +49,43 @@ Content Wrapper. Contains page content -->
                <div class="row">
                     <div class="col-sm-6">
                       <div id="example1_length" class="dataTables_length">
-                        <label>
-                          Cari Berdasarkan 
-                          <select class="form-control input-sm" aria-controls="example1" name="example1_length">
-                            <option value="Nim">Nim</option>
-                            <option value="Nama">Nama</option>
-                          </select>
-                          </label>
+
                         </div>
                       </div>
                 </div><!--/row-->
 
                 <div class="row">
                      <div class="col-sm-4">
-                      <div class="input-group input-group-sm">
-                        <input class="form-control" type="text"></input>
-                        <span class="input-group-btn">
-                         <button class="btn btn-info btn-flat" type="button">GO</button>
-                        </span>
+                       <label>
+                         Cari Berdasarkan
+                       </label>
+                      <div class="form-group">
+                        <input class="form-control" type="text" placeholder="Nim atau Nama"></input>
                       </div>
+                        <button class="btn btn-success"><i class="fa fa-search"></i> Cari</button>
                     </div>
                 </div>
 
             <div class="box box-info" style="margin-top:20px">
               <div class="box-body">
-                <section class="invoice">
-          <!-- title row -->
-          <div class="row">
-            <div class="col-xs-12">
-              <h2 class="page-header">
-                <i class="fa fa-globe"></i> Nota SPP
-                <small class="pull-right">Tanggal Transaksi: 2/10/2014</small>
-              </h2>
-            </div><!-- /.col -->
-          </div>
-          <!-- info row -->
-          <div class="row invoice-info">
-            <div class="col-sm-4 invoice-col">
-              <address>
-                <strong>Aziz Akhmad Wardoyo</strong><br>
-                <b>NIS : 30891 </b><br>
-                <b>Kelas : XI </b><br>
-              </address>
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-
-          <!-- Table row -->
-          <div class="row">
-            <div class="col-xs-12 table-responsive">
-              <table class="table table-striped">
-                <thead>
-                  <tr>
-                    <th>No</th>
-                    <th>Komponen</th>
-                    <th>Subtotal</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td>Internet</td>
-                    <td>Rp 25.000</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>Sumbangan Gedung</td>
-                    <td>Rp 35.000</td>
-                  </tr>
-                  <tr>
-                    <td>3</td>
-                    <td>Wisata</td>
-                    <td>Rp 25.000</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-
-          <div class="table-responsive" style="margin-left:370px">
-                <table class="table">
-                  <tbody><tr>
-                    <th style="width:50%">Subtotal:</th>
-                    <td>Rp 85.000</td>
-                  </tr>
-                  <tr>
-                    <th>Untuk Pembayaran Bulan:</th>
-                    <td>
-                      <input class="form-control" placeholder=".col-xs-5" type="text">
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>Dana Bos :</th>
-                    <td><input class="form-control" placeholder=".col-xs-5" type="text"></td>
-                  </tr>
-                  <tr>
-                    <th>Total:</th>
-                    <td>Rp 85.000</td>
-                  </tr>
-                </tbody></table>
-              </div>
-
-         <!--  <div class="row" >
-            <div class="col-xs-6">
-              
-            </div>
-          </div> --> 
-
-          <!-- this row will not appear when printing -->
-          <div class="row no-print">
-            <div class="col-xs-12">
-              <a href="invoice-print.html" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
-              <button class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Submit Payment</button>
-              <button class="btn btn-primary pull-right" style="margin-right: 5px;"><i class="fa fa-download"></i> Generate PDF</button>
-            </div>
-          </div>
-        </section>
-
+                <table id="example1" class="table table-bordered table-striped">
+                     <thead>
+                       <tr>
+                         <th>Nis</th>
+                         <th>Nama</th>
+                         <th>Kelas</th>
+                         <th>Action</th>
+                       </tr>
+                     </thead>
+                     <tbody>
+                       <tr>
+                         <td></td>
+                         <td></td>
+                         <td></td>
+                         <td></td>
+                       </tr>
+                     </tbody>
+                 </table>
               </div><!-- /.box body-->
             </div>
           </div>
--- sas2.sql
+++ sas2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.12
+-- version 4.2.12deb2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 12, 2015 at 10:26 AM
--- Server version: 5.6.25
--- PHP Version: 5.6.11
+-- Host: localhost
+-- Generation Time: Oct 04, 2015 at 08:03 PM
+-- Server version: 5.6.25-0ubuntu0.15.04.1
+-- PHP Version: 5.6.4-4ubuntu6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `sas2`
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `bos` (
-  `id_bos` int(11) NOT NULL,
+`id_bos` int(11) NOT NULL,
   `id_siswa_kelas` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -39,13 +39,10 @@ CREATE TABLE IF NOT EXISTS `bos` (
 --
 CREATE TABLE IF NOT EXISTS `cek_smester` (
 `idtahun` int(10)
-,`nama_smester` varchar(30)
-,`awal_smester` date
-,`akhir_smester` date
-,`jenis_smester` varchar(20)
+,`awal_tahun_pelajaran` date
+,`akhir_tahun_pelajaran` date
 ,`tahun_pelajaran` varchar(11)
 );
-
 -- --------------------------------------------------------
 
 --
@@ -53,23 +50,45 @@ CREATE TABLE IF NOT EXISTS `cek_smester` (
 --
 
 CREATE TABLE IF NOT EXISTS `kelas` (
-  `idkelas` int(10) NOT NULL COMMENT 'id kode kelas',
+`idkelas` int(10) NOT NULL COMMENT 'id kode kelas',
+  `jenis_kelas` int(2) NOT NULL,
   `namakelas` varchar(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kelas`
 --
 
-INSERT INTO `kelas` (`idkelas`, `namakelas`) VALUES
-(1, 'Array'),
-(2, 'X5'),
-(3, 'X6'),
-(4, 'X7'),
-(5, 'XI IPA 1'),
-(6, 'XI IPA 2'),
-(7, 'XI IPS 1'),
-(8, 'XI IPS 2');
+INSERT INTO `kelas` (`idkelas`, `jenis_kelas`, `namakelas`) VALUES
+(1, 1, 'X1'),
+(2, 1, 'X2'),
+(3, 1, 'X3'),
+(4, 1, 'X4'),
+(5, 1, 'X5'),
+(6, 1, 'X6'),
+(7, 1, 'X7'),
+(8, 1, 'X8'),
+(9, 1, 'X9'),
+(10, 2, 'XI IPA 1'),
+(11, 2, 'XI IPA 2'),
+(12, 2, 'XI IPA 3'),
+(13, 2, 'XI IPA 4'),
+(14, 2, 'XI IPA 5'),
+(15, 2, 'XI IPS 1'),
+(16, 2, 'XI IPS 2'),
+(17, 2, 'XI IPS 3'),
+(18, 2, 'XI IPS 4'),
+(19, 2, 'XI IPS 5'),
+(20, 3, 'XII IPA 1'),
+(21, 3, 'XII IPA 2'),
+(22, 3, 'XII IPA 3'),
+(23, 3, 'XII IPA 4'),
+(24, 3, 'XII IPA 5'),
+(25, 3, 'XII IPS 1'),
+(26, 3, 'XII IPS 2'),
+(27, 3, 'XII IPS 3'),
+(28, 3, 'XII IPS 4'),
+(29, 3, 'XII IPS 5');
 
 -- --------------------------------------------------------
 
@@ -78,11 +97,23 @@ INSERT INTO `kelas` (`idkelas`, `namakelas`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `komponen` (
-  `idkomponen` int(10) NOT NULL,
+`idkomponen` int(10) NOT NULL,
   `nama_komp` varchar(100) NOT NULL,
   `deskripsi` text NOT NULL,
-  `status` varchar(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `iuran` int(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `komponen`
+--
+
+INSERT INTO `komponen` (`idkomponen`, `nama_komp`, `deskripsi`, `iuran`) VALUES
+(6, 'internet', 'biaya internet', 20000),
+(13, 'iuran biasa', 'default', 10),
+(14, 'asdfghsdfg', 'dfbrb', 10000),
+(15, 'huhuhahahsdhushuda', 'qwqwqw', 10000),
+(16, 'asd', 'asdsasdsasd', 10),
+(17, 'komputer', 'komputer yang digunakan untuk keperluan belajar mengajar di dalamm lab maupun yang ada di halaman sekolah   skldhaslfjbwksjdfbsld\nvsdvsodjfbvlksrdg\nvrsdg\nvs\ndgv\ndfb\nrsdhfvdjfcvs\ndvdf\ncv\ndfcb\ndomputer yang digunakan untuk keperluan belajar mengajar di dalamm lab maupun yang ada di halaman sekolah   skldhaslfjbwksjdfbsld\nvsdvsodjfbvlksrdg\nvrsdg\nvs\ndgv\ndfb\nrsdhfvdjfcvs\ndvdf\ncv\ndfcb\ndomputer yang digunakan untuk keperluan belajar mengajar di dalamm lab maupun yang ada di halaman sekolah   skldhaslfjbwksjdfbsld\nvsdvsodjfbvlksrdg\nvrsdg\nvs\ndgv\ndfb\nrsdhfvdjfcvs\ndvdf\ncv\ndfcb\ndomputer yang digunakan untuk keperluan belajar mengajar di dalamm lab maupun yang ada di halaman sekolah   skldhaslfjbwksjdfbsld\nvsdvsodjfbvlksrdg\nvrsdg\nvs\ndgv\ndfb\nrsdhfvdjfcvs\ndvdf\ncv\ndfcb\ndomputer yang digunakan untuk keperluan belajar mengajar di dalamm lab maupun yang ada di halaman sekolah   skldhaslfjbwksjdfbsld\nvsdvsodjfbvlksrdg\nvrsdg\nvs\ndgv\ndfb\nrsdhfvdjfcvs\ndvdf\ncv\ndfcb\ndomputer yang digunakan untuk keperluan belajar mengajar di dalamm lab maupun yang ada di halaman sekolah   skldhaslfjbwksjdfbsld\nvsdvsodjfbvlksrdg\nvrsdg\nvs\ndgv\ndfb\nrsdhfvdjfcvs\ndvdf\ncv\ndfcb\ndomputer yang digunakan untuk keperluan belajar mengajar di dalamm lab maupun yang ada di halaman sekolah   skldhaslfjbwksjdfbsld\nvsdvsodjfbvlksrdg\nvrsdg\nvs\ndgv\ndfb\nrsdhfvdjfcvs\ndvdf\ncv\ndfcb\ndomputer yang digunakan untuk keperluan belajar mengajar di dalamm lab maupun yang ada di halaman sekolah   skldhaslfjbwksjdfbsld\nvsdvsodjfbvlksrdg\nvrsdg\nvs\ndgv\ndfb\nrsdhfvdjfcvs\ndvdf\ncv\ndfcb\nd\n ', 100000000);
 
 -- --------------------------------------------------------
 
@@ -91,10 +122,39 @@ CREATE TABLE IF NOT EXISTS `komponen` (
 --
 
 CREATE TABLE IF NOT EXISTS `komponen_setting` (
-  `idkompsetting` int(10) NOT NULL,
-  `idkomp` int(10) NOT NULL,
+`idkompsetting` int(10) NOT NULL,
+  `idkomponen` int(10) NOT NULL,
   `iuran` int(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `komponen_setting`
+--
+
+INSERT INTO `komponen_setting` (`idkompsetting`, `idkomponen`, `iuran`) VALUES
+(1, 1, 200000),
+(2, 2, 40000),
+(3, 4, 30000),
+(4, 5, 5000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+
+CREATE TABLE IF NOT EXISTS `level` (
+  `idlevel` int(10) NOT NULL,
+  `namalevel` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `level`
+--
+
+INSERT INTO `level` (`idlevel`, `namalevel`) VALUES
+(1, 'root'),
+(2, 'admin');
 
 -- --------------------------------------------------------
 
@@ -103,7 +163,7 @@ CREATE TABLE IF NOT EXISTS `komponen_setting` (
 --
 
 CREATE TABLE IF NOT EXISTS `siswa` (
-  `idsiswa` int(10) NOT NULL COMMENT 'id table',
+`idsiswa` int(10) NOT NULL COMMENT 'id table',
   `nim` int(20) NOT NULL COMMENT 'nim siswa',
   `namasiswa` varchar(50) NOT NULL,
   `gender` enum('L','P') NOT NULL,
@@ -125,7 +185,7 @@ INSERT INTO `siswa` (`idsiswa`, `nim`, `namasiswa`, `gender`, `alamat`, `tmlahir
 (5, 190867, 'Amin', 'L', 'Bantul', '0', '1994-02-12', '5', 'Dhoni'),
 (6, 31350, 'Jeny', 'P', 'Kaliurang', '0', '1994-02-11', '5', 'Deni'),
 (7, 56789, 'Ria', 'P', 'Paris', '0', '1994-03-15', '5', 'Hadi'),
-(8, 27890, 'Riki', 'L', 'Garut', 'Bandung', '1994-05-12', '6', 'Toni'),
+(8, 27890, 'Riki', '', 'Garut josss\r\n', 'Bandung', '1994-05-12', '', 'Toni blank'),
 (9, 37098, 'Rio', 'L', 'Magelang', '0', '1994-02-11', '5', 'Bambang'),
 (12, 14567, 'Rere', 'P', 'Ponorogo', 'Madiun', '2015-09-07', '', 'Prapto');
 
@@ -139,7 +199,6 @@ CREATE TABLE IF NOT EXISTS `siswa_belum_dapat_kelas` (
 ,`nim` int(20)
 ,`namasiswa` varchar(50)
 );
-
 -- --------------------------------------------------------
 
 --
@@ -147,18 +206,23 @@ CREATE TABLE IF NOT EXISTS `siswa_belum_dapat_kelas` (
 --
 
 CREATE TABLE IF NOT EXISTS `siswa_kelas` (
-  `id_siswa_kelas` int(10) NOT NULL,
+`id_siswa_kelas` int(10) NOT NULL,
   `idsiswa` int(10) NOT NULL,
   `idkelas` int(10) NOT NULL,
   `idtahun` int(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `siswa_kelas`
 --
 
 INSERT INTO `siswa_kelas` (`id_siswa_kelas`, `idsiswa`, `idkelas`, `idtahun`) VALUES
-(9, 8, 5, 6);
+(8, 6, 6, 6),
+(9, 8, 5, 6),
+(10, 1, 2, 6),
+(11, 3, 1, 6),
+(12, 5, 1, 6),
+(13, 9, 1, 6);
 
 -- --------------------------------------------------------
 
@@ -177,7 +241,6 @@ CREATE TABLE IF NOT EXISTS `siswa_sudah_punya_kelas` (
 ,`tgllahir` date
 ,`tahun_pelajaran` varchar(11)
 );
-
 -- --------------------------------------------------------
 
 --
@@ -185,7 +248,7 @@ CREATE TABLE IF NOT EXISTS `siswa_sudah_punya_kelas` (
 --
 
 CREATE TABLE IF NOT EXISTS `spp` (
-  `idspp` int(10) NOT NULL,
+`idspp` int(10) NOT NULL,
   `tanggal` date NOT NULL,
   `transaksi` varchar(20) NOT NULL,
   `idtahun` int(11) NOT NULL,
@@ -201,7 +264,7 @@ CREATE TABLE IF NOT EXISTS `spp` (
 --
 
 CREATE TABLE IF NOT EXISTS `spp_detail` (
-  `idsppdetail` int(10) NOT NULL,
+`idsppdetail` int(10) NOT NULL,
   `idspp` int(10) NOT NULL,
   `idkomp` int(10) NOT NULL,
   `jumlah` int(10) NOT NULL
@@ -214,12 +277,37 @@ CREATE TABLE IF NOT EXISTS `spp_detail` (
 --
 
 CREATE TABLE IF NOT EXISTS `spp_setting` (
-  `idsppsetting` int(10) NOT NULL,
-  `idkelas` int(10) NOT NULL,
+`idsppsetting` int(10) NOT NULL,
+  `jenis_kelas` int(10) NOT NULL,
   `idkomponen` int(10) NOT NULL,
   `idtahun` int(10) NOT NULL,
   `periode` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `spp_setting`
+--
+
+INSERT INTO `spp_setting` (`idsppsetting`, `jenis_kelas`, `idkomponen`, `idtahun`, `periode`) VALUES
+(1, 1, 1, 6, 'juli'),
+(2, 2, 1, 6, 'juli'),
+(3, 2, 2, 6, 'juli'),
+(4, 3, 2, 1, 'juli'),
+(5, 3, 1, 1, 'juli'),
+(6, 3, 6, 6, 'juli'),
+(7, 1, 6, 6, 'Maret'),
+(8, 1, 6, 6, 'Maret'),
+(9, 1, 13, 6, 'Maret'),
+(10, 3, 6, 6, 'Juni'),
+(11, 3, 13, 6, 'Juni'),
+(12, 3, 14, 6, 'Juni'),
+(13, 3, 15, 6, 'Juni'),
+(14, 3, 16, 6, 'Juni'),
+(15, 1, 6, 6, 'Juli'),
+(16, 1, 13, 6, 'Juli'),
+(17, 2, 14, 6, 'Juli'),
+(18, 2, 15, 6, 'Juli'),
+(19, 2, 16, 6, 'Juli');
 
 -- --------------------------------------------------------
 
@@ -228,11 +316,9 @@ CREATE TABLE IF NOT EXISTS `spp_setting` (
 --
 
 CREATE TABLE IF NOT EXISTS `tahun` (
-  `idtahun` int(10) NOT NULL,
-  `nama_smester` varchar(30) NOT NULL,
-  `awal_smester` date NOT NULL,
-  `akhir_smester` date NOT NULL,
-  `jenis_smester` varchar(20) NOT NULL,
+`idtahun` int(10) NOT NULL,
+  `awal_tahun_pelajaran` date NOT NULL,
+  `akhir_tahun_pelajaran` date NOT NULL,
   `tahun_pelajaran` varchar(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
@@ -240,13 +326,45 @@ CREATE TABLE IF NOT EXISTS `tahun` (
 -- Dumping data for table `tahun`
 --
 
-INSERT INTO `tahun` (`idtahun`, `nama_smester`, `awal_smester`, `akhir_smester`, `jenis_smester`, `tahun_pelajaran`) VALUES
-(1, 'Genap', '2015-09-04', '2016-04-01', 'Genap', '2015'),
-(2, 'Ganjil', '2015-09-04', '2016-05-07', 'Ganjil', '2015'),
-(3, 'Ganjil', '2015-09-04', '2016-02-04', 'Ganjil', '2015/2016'),
-(4, 'Genap', '2015-09-04', '2016-04-04', 'Genap', '2015/2016'),
-(5, 'Genap', '2016-07-01', '2017-07-01', 'Genap', '2016/2017'),
-(6, 'Ganjil', '2017-07-01', '2018-07-01', 'Ganjil', '2017/2018');
+INSERT INTO `tahun` (`idtahun`, `awal_tahun_pelajaran`, `akhir_tahun_pelajaran`, `tahun_pelajaran`) VALUES
+(1, '2015-09-04', '2016-04-01', '2015'),
+(2, '2015-09-04', '2016-05-07', '2015'),
+(3, '2015-09-04', '2016-02-04', '2015/2016'),
+(4, '2015-09-04', '2016-04-04', '2015/2016'),
+(5, '2016-07-01', '2017-07-01', '2016/2017'),
+(6, '2017-07-01', '2018-07-01', '2017/2018');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `tampil_user`
+--
+CREATE TABLE IF NOT EXISTS `tampil_user` (
+`iduser` int(10)
+,`username` varchar(30)
+,`password` varchar(30)
+,`namalevel` varchar(20)
+);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `iduser` int(10) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `idlevel` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`iduser`, `username`, `password`, `idlevel`) VALUES
+(1, 'root', 'root', 1),
+(2, 'admin', 'admin', 2);
 
 -- --------------------------------------------------------
 
@@ -255,7 +373,7 @@ INSERT INTO `tahun` (`idtahun`, `nama_smester`, `awal_smester`, `akhir_smester`,
 --
 DROP TABLE IF EXISTS `cek_smester`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `cek_smester` AS select `tahun`.`idtahun` AS `idtahun`,`tahun`.`nama_smester` AS `nama_smester`,`tahun`.`awal_smester` AS `awal_smester`,`tahun`.`akhir_smester` AS `akhir_smester`,`tahun`.`jenis_smester` AS `jenis_smester`,`tahun`.`tahun_pelajaran` AS `tahun_pelajaran` from `tahun` order by `tahun`.`idtahun` desc limit 1;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `cek_smester` AS select `tahun`.`idtahun` AS `idtahun`,`tahun`.`awal_tahun_pelajaran` AS `awal_tahun_pelajaran`,`tahun`.`akhir_tahun_pelajaran` AS `akhir_tahun_pelajaran`,`tahun`.`tahun_pelajaran` AS `tahun_pelajaran` from `tahun` order by `tahun`.`idtahun` desc limit 1;
 
 -- --------------------------------------------------------
 
@@ -275,6 +393,15 @@ DROP TABLE IF EXISTS `siswa_sudah_punya_kelas`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `siswa_sudah_punya_kelas` AS select `siswa`.`idsiswa` AS `idsiswa`,`siswa`.`nim` AS `nim`,`siswa`.`namasiswa` AS `namasiswa`,`kelas`.`namakelas` AS `namakelas`,`siswa`.`alamat` AS `alamat`,`siswa`.`namawali` AS `namawali`,`siswa`.`gender` AS `gender`,`siswa`.`tmlahir` AS `tmlahir`,`siswa`.`tgllahir` AS `tgllahir`,`tahun`.`tahun_pelajaran` AS `tahun_pelajaran` from (((`siswa` join `kelas`) join `siswa_kelas`) join `tahun`) where ((`siswa_kelas`.`idsiswa` = `siswa`.`idsiswa`) and (`siswa_kelas`.`idkelas` = `kelas`.`idkelas`) and (`siswa_kelas`.`idtahun` = `tahun`.`idtahun`)) order by `tahun`.`tahun_pelajaran` desc;
 
+-- --------------------------------------------------------
+
+--
+-- Structure for view `tampil_user`
+--
+DROP TABLE IF EXISTS `tampil_user`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `tampil_user` AS select `user`.`iduser` AS `iduser`,`user`.`username` AS `username`,`user`.`password` AS `password`,`level`.`namalevel` AS `namalevel` from (`user` join `level`) where (`user`.`idlevel` = `level`.`idlevel`);
+
 --
 -- Indexes for dumped tables
 --
@@ -283,62 +410,73 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- Indexes for table `bos`
 --
 ALTER TABLE `bos`
-  ADD PRIMARY KEY (`id_bos`);
+ ADD PRIMARY KEY (`id_bos`);
 
 --
 -- Indexes for table `kelas`
 --
 ALTER TABLE `kelas`
-  ADD PRIMARY KEY (`idkelas`);
+ ADD PRIMARY KEY (`idkelas`);
 
 --
 -- Indexes for table `komponen`
 --
 ALTER TABLE `komponen`
-  ADD PRIMARY KEY (`idkomponen`);
+ ADD PRIMARY KEY (`idkomponen`);
 
 --
 -- Indexes for table `komponen_setting`
 --
 ALTER TABLE `komponen_setting`
-  ADD PRIMARY KEY (`idkompsetting`);
+ ADD PRIMARY KEY (`idkompsetting`);
+
+--
+-- Indexes for table `level`
+--
+ALTER TABLE `level`
+ ADD PRIMARY KEY (`idlevel`);
 
 --
 -- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`idsiswa`),
-  ADD UNIQUE KEY `nim` (`nim`);
+ ADD PRIMARY KEY (`idsiswa`), ADD UNIQUE KEY `nim` (`nim`);
 
 --
 -- Indexes for table `siswa_kelas`
 --
 ALTER TABLE `siswa_kelas`
-  ADD PRIMARY KEY (`id_siswa_kelas`);
+ ADD PRIMARY KEY (`id_siswa_kelas`);
 
 --
 -- Indexes for table `spp`
 --
 ALTER TABLE `spp`
-  ADD PRIMARY KEY (`idspp`);
+ ADD PRIMARY KEY (`idspp`);
 
 --
 -- Indexes for table `spp_detail`
 --
 ALTER TABLE `spp_detail`
-  ADD PRIMARY KEY (`idsppdetail`);
+ ADD PRIMARY KEY (`idsppdetail`);
 
 --
 -- Indexes for table `spp_setting`
 --
 ALTER TABLE `spp_setting`
-  ADD PRIMARY KEY (`idsppsetting`);
+ ADD PRIMARY KEY (`idsppsetting`);
 
 --
 -- Indexes for table `tahun`
 --
 ALTER TABLE `tahun`
-  ADD PRIMARY KEY (`idtahun`);
+ ADD PRIMARY KEY (`idtahun`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`iduser`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -348,52 +486,52 @@ ALTER TABLE `tahun`
 -- AUTO_INCREMENT for table `bos`
 --
 ALTER TABLE `bos`
-  MODIFY `id_bos` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id_bos` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `idkelas` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id kode kelas',AUTO_INCREMENT=9;
+MODIFY `idkelas` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id kode kelas',AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `komponen`
 --
 ALTER TABLE `komponen`
-  MODIFY `idkomponen` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `idkomponen` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `komponen_setting`
 --
 ALTER TABLE `komponen_setting`
-  MODIFY `idkompsetting` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `idkompsetting` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `idsiswa` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id table',AUTO_INCREMENT=13;
+MODIFY `idsiswa` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id table',AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `siswa_kelas`
 --
 ALTER TABLE `siswa_kelas`
-  MODIFY `id_siswa_kelas` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+MODIFY `id_siswa_kelas` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `spp`
 --
 ALTER TABLE `spp`
-  MODIFY `idspp` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `idspp` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `spp_detail`
 --
 ALTER TABLE `spp_detail`
-  MODIFY `idsppdetail` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `idsppdetail` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `spp_setting`
 --
 ALTER TABLE `spp_setting`
-  MODIFY `idsppsetting` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `idsppsetting` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `tahun`
 --
 ALTER TABLE `tahun`
-  MODIFY `idtahun` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `idtahun` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
